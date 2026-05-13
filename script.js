2be636f7e7623e2498f1bacda931465087643d89
Forsbergoscar1 <forsbergoscar1@gmail.com>
2026-05-13 18:33:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Läkarjournaler sidan är igång");
+const searchInput = document.getElementById("searchInput");
+
+if (searchInput) {
+
+    searchInput.addEventListener("keyup", function () {
+
+        let filter = searchInput.value.toLowerCase();
+
+        let cards = document.querySelectorAll(".journal-card");
+
+        cards.forEach(card => {
+
+            let text = card.innerText.toLowerCase();
+
+            if (text.includes(filter)) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+
+        });
+
+    });
+
+}
